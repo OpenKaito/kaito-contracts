8716b4e17b58c5cd3e11f006bde8159e8758bf00
Charles Jhong <charlesjhongc@gmail.com>
2025-02-06 00:26:01 +0800

    update total supply limit

--- contracts/Kaito.sol
+++ contracts/Kaito.sol
@@ -6,7 +6,7 @@ import "openzeppelin/access/Ownable2Step.sol";
 
 /// @title Kaito
 contract Kaito is ERC20Permit, Ownable2Step {
-    uint256 constant TOTALSUPPLY = 1e9;
+    uint256 constant TOTALSUPPLY = 1e27;
     constructor(string memory name_, string memory symbol_, address[] memory wallets, uint256[] memory amounts) ERC20Permit(name_) ERC20(name_, symbol_) {
         require(wallets.length == amounts.length, "array_length");
         for (uint256 i = 0; i < wallets.length; i++) {
