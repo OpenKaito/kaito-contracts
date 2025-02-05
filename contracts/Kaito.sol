5b596891a669d9718be08b4a6ddbb8b700c20b27
Charles Jhong <charlesjhongc@gmail.com>
2025-02-06 00:30:40 +0800

    remove ownable

--- contracts/Kaito.sol
+++ contracts/Kaito.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.19;
 
 import "openzeppelin/token/ERC20/extensions/ERC20Permit.sol";
-import "openzeppelin/access/Ownable2Step.sol";
 
 /// @title Kaito
-contract Kaito is ERC20Permit, Ownable2Step {
+contract Kaito is ERC20Permit {
     uint256 constant TOTALSUPPLY = 1e27;
     constructor(string memory name_, string memory symbol_, address[] memory wallets, uint256[] memory amounts) ERC20Permit(name_) ERC20(name_, symbol_) {
         require(wallets.length == amounts.length, "array_length");
@@ -13,10 +12,6 @@ contract Kaito is ERC20Permit, Ownable2Step {
             _mint(wallets[i], amounts[i]);
         }
         require(totalSupply() == TOTALSUPPLY, "supply_mismatch");
-    }
-
-    function renounceOwnership() public view override onlyOwner {
-        revert();
     }
 
     function burn(uint256 amount) external {
